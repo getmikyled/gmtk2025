cb2efffb0910a19138b5c629f3fb9e6fade49f4e
Chanelle <25163297+chmosquera@users.noreply.github.com>
2025-08-02 19:40:44 -0700

    Created function to show dialogue, only does debug log, but can be updated to show the text

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,6 +1,74 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
+    public static DialogueManager Instance;
     
+    [SerializeField] private string dialoguePath1 = "DialogueData/Course1";
+    [SerializeField] private string dialoguePath2 = "DialogueData/Course2";
+    [SerializeField] private string dialoguePath3 = "DialogueData/Course3";
+    
+    public List<DialogueData> dialogueData;
+    public Dictionary<string, DialogueData> dialogueMap = new Dictionary<string, DialogueData>();
+
+    public PlayerEnum winningPlayer;
+    public PlayerEnum losingPlayer;
+    
+    private bool isDialogueActive = false;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.Log("Instance of ReplayManager already exists. Destroying this  object.");
+            return;
+        }
+        
+        Instance = this;
+
+        // Load dialogue in the dictionary so they can be played
+        LoadDialogue(dialoguePath1);
+        // LoadDialogue(dialoguePath2);
+        // LoadDialogue(dialoguePath3);
+    }
+
+    public void LoadDialogue(string folderName)
+    {
+        DialogueData[] loadedData = Resources.LoadAll<DialogueData>(folderName);
+        foreach (var item in loadedData)
+        {
+            dialogueMap.Add(item.id, item);
+            dialogueData.Add(item);
+        }
+    }
+    
+    public Coroutine ShowDialogue(string id, float duration = 3f)
+    {
+        return StartCoroutine(ShowDialogueCoroutine(id, duration));
+    }
+
+    private IEnumerator ShowDialogueCoroutine(string id, float duration)
+    {
+        // Wait until previous dialogue finishes
+        while (isDialogueActive)
+            yield return null;
+
+        if (!dialogueMap.ContainsKey(id))
+        {
+            Debug.LogWarning($"Dialogue ID '{id}' not found.");
+            yield break;
+        }
+
+        isDialogueActive = true;
+
+        DialogueData dialogue = dialogueMap[id];
+        Debug.Log($"{dialogue.character}: {dialogue.textString}");
+
+        yield return new WaitForSeconds(duration);
+
+        isDialogueActive = false;
+    }
 }
---
+++ Assets/Scripts/Test/DialogueTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    
+public class DialogueTest : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(PlayDialogueSequence());
+        
+    }
+
+    public IEnumerator PlayDialogueSequence()
+    {
+        yield return DialogueManager.Instance.ShowDialogue("course_0_hole_0_0");
+        yield return DialogueManager.Instance.ShowDialogue("course_0_hole_0_1");
+    }
+}
